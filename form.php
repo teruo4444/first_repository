c20860946cf21b196df5b32c77a2dab75d405f39
asato <s.teruo4444@gmail.com>
2019-06-13 20:07:35 +0900

    re_kadai3_add

--- form.php
+++ form.php
@@ -8,7 +8,12 @@
 <p>足し算</p>
 <form method="GET" action='display.php'>
 <input type='text' name='num1'>
-<p>+</p>
+<select name='calculate'>
+<option value='plus'>+</option>
+<option value='minus'>-</option>
+<option value='multi'>x</option>
+<option value='divid'>/</option>
+</select>
 <input type='text' name='num2'>
 <input type='submit'>
 </form>
