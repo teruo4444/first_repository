8ff455d210910606ac7b17d1dcc5439ad11b67c3
asato <s.teruo4444@gmail.com>
2019-06-13 19:30:30 +0900

    re_kadai

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form method="POST" action='display.php'>
+<input type='text' name='name' placeholder='なまえ'>
+<input type='submit'>
+</form>
+</body>
